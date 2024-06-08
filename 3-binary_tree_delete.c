0717564bbc9b9758ed7f6f9f83d35cf1fce24842
ademitidayo <adenijibimmy08@gmail.com>
2024-06-08 04:01:23 +0000

    indents

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -7,13 +7,13 @@
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return;
+	if (tree == NULL)
+		return;
 
-    /* Recursively delete the left and right subtrees */
-    binary_tree_delete(tree->left);
-    binary_tree_delete(tree->right);
+	/* Recursively delete the left and right subtrees */
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
 
-    /* Free the root node */
-    free(tree);
+	/* Free the root node */
+	free(tree);
 }
